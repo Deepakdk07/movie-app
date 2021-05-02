e357d2f0792afbe605a0b427d5d3dd8992dff617
Deepak Kumar <deadlydeepak9@gmail.com>
2021-05-02 22:51:04 +0530

    fourth

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,6 @@
-
 module.exports = {
   plugins: {
-     tailwindcss: {},
-     autoprefixer: {},
-     ...(process.env.NODE_ENV === 'production'
-        ? {
-             '@fullhuman/postcss-purgecss': {
-                // added sections folder and changed extension to jsx
-                content: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './sections/**/**/*.{js,ts,jsx,tsx}'],
-                defaultExtractor: content =>
-                   content.match(/[\w-/:]+(?<!:)/g) || [],
-             },
-          }
-        : {}),
+    tailwindcss: {},
+    autoprefixer: {},
   },
 }
